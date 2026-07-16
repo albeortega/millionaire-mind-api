847ae06d1b2f467ee985b613a67ed3f2e73ba49c
albeortega <albeortega@gmail.com>
2026-07-15 20:23:12 -0400

    Fix chat service constructor injection

--- src/main/java/com/niloortega/millionairemind/service/ChatService.java
+++ src/main/java/com/niloortega/millionairemind/service/ChatService.java
@@ -11,6 +11,7 @@ import java.time.Clock;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,6 +26,7 @@ public class ChatService {
 	private final MessageRepository messageRepository;
 	private final Clock clock;
 
+	@Autowired
 	public ChatService(ConversationRepository conversationRepository, MessageRepository messageRepository) {
 		this(conversationRepository, messageRepository, Clock.systemUTC());
 	}
